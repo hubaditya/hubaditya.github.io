fba9af5b6641fe042d1d848232785fa029586966
Aditya Srivastava <beingaditya95@gmail.com>
2022-02-18 16:15:15 -0500

    Updates

---
+++ precache-manifest.2b463a71152e608849fa2b7442b9a748.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec3b69687b0f3034640061770e49e6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f2c88728da7f73de3ae",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "cf917c9363ccd8badb7d",
+    "url": "/static/js/2.9e51c511.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.9e51c511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f2c88728da7f73de3ae",
+    "url": "/static/js/main.03913f43.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "1ca0ce94826f4f09583e6b879538d807",
+    "url": "/static/media/cincinnatireds-icon.1ca0ce94.png"
+  },
+  {
+    "revision": "99fe0fb48527e6b4e58d3c35b8cb4f50",
+    "url": "/static/media/dp.99fe0fb4.jpeg"
+  },
+  {
+    "revision": "4942ac2d887f510ac973e3a20f5adbf4",
+    "url": "/static/media/essenceglobal-icon.4942ac2d.png"
+  },
+  {
+    "revision": "4cd8ca2fa0b4913ded5854edf6bf4283",
+    "url": "/static/media/resume.4cd8ca2f.pdf"
+  },
+  {
+    "revision": "89ea113cbed1ada00979b66025b63108",
+    "url": "/static/media/unyscape-icon.89ea113c.png"
+  },
+  {
+    "revision": "21d1b846c426f40152e1808a3cd1beb2",
+    "url": "/static/media/wiseradvisor-icon.21d1b846.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.46b33a434c14e5eb2e1aff04eddfcf97.js"
+  "/precache-manifest.2b463a71152e608849fa2b7442b9a748.js"
 );
 
 self.addEventListener('message', (event) => {
